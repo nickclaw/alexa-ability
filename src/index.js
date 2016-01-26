71a054a8816b592fca8bd533e8ea42f9b2a9bf77
Nicholas Clawson <nickclaw@gmail.com>
2016-01-26 01:01:26 -0800

    Don't shim Promise

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
-import { Promise } from 'es6-promise';
 export { Ability } from './Ability';
 export * as intent from './amazon-intents';
-
-if (!global.Promise) global.Promise = Promise;
--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,11 @@
 import chai from 'chai';
 import sinon from 'sinon';
 import chaiAsPromised from 'chai-as-promised';
+import { Promise } from 'es6-promise';
+
+if (!global.Promise) { // shim Promise
+    global.Promise = Promise;
+}
 
 chai.should();
 chai.use(chaiAsPromised);
