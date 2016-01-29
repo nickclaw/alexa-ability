451c45b524168a11702de0ffc39ee6843c10f711
Nicholas Clawson <nickclaw@gmail.com>
2016-01-28 23:42:56 -0800

    Allow Requests to handle ssml, add tests

--- src/Request.js
+++ src/Request.js
@@ -1,60 +1,73 @@
 import get from 'lodash/get';
 import omit from 'lodash/omit';
+import { renderToString } from 'alexa-ssml';
 
 export class Request {
 
     constructor(event) {
         this._event = event;
 
+        this.isNew = get(event, 'session.new', false);
         this.version = get(event, 'version', '1.0');
-        this.session = get(event, 'session', {});
+        this.session = get(event, 'session.session', {});
         this.params = get(event, 'request.intent.slots', {});
+
+        this._speech = null;
+        this._card = null;
+        this._reprompt = null;
+        this._end = false;
     }
 
-    say(text) {
-        this._speech = text;
+    say(content) {
+        this._speech = content;
         return this;
     }
 
-    show(text) {
-        this._card = text;
+    show(content) {
+        this._card = content;
         return this;
     }
 
-    reprompt(text) {
-        this._reprompt = text;
+    reprompt(content) {
+        this._reprompt = content;
         return this;
     }
 
     end() {
         this._end = true;
+        return this;
     }
 
     toJSON() {
         const data = {
             version: this.version,
-            sessionAttributes: omit(this.session, []),
+            sessionAttributes: this.session,
             response: { shouldEndSession: this._end }
         };
 
-        if (this._speech) data.response.outputSpeech = {
-            type: "PlainText",
-            text: this._speech
-        };
+        if (this._speech)
+            data.response.outputSpeech = toSpeechResponse(this._speech);
 
-        if (this._card) data.response.card = {
-            type: "Simple",
-            title: "Response",
-            content: this._card
-        };
+        if (this._card)
+            data.response.card = {
+                type: "Simple",
+                title: "Response",
+                content: this._card
+            };
 
-        if (this._reprompt) data.response.reprompt = {
-            outputSpeech: {
-                type: "PlainText",
-                text: this._reprompt
-            }
-        };
+        if (this._reprompt)
+            data.response.reprompt = {
+                outputSpeech: toSpeechResponse(this._reprompt)
+            };
 
         return data;
     }
+}
+
+function toSpeechResponse(content) {
+    const isTag = get(content, 'tag');
+
+    return isTag ?
+        { type: "SSML", text: renderToString(content) } :
+        { type: "PlainText", text: content } ;
 }
---
+++ test/request-test.js
@@ -0,0 +1,112 @@
+/** @jsx ssml */
+
+import { ssml } from 'alexa-ssml';
+import { Request } from '../src/Request';
+
+const intentRequest = require('./fixtures/intent-request');
+const intentResponse = require('./fixtures/intent-response');
+
+
+describe('Request', function() {
+
+    let req = null;
+
+    beforeEach(function() {
+        req = new Request(intentRequest);
+    });
+
+    describe('object', function() {
+
+        it('should have a "isNew" field', function() {
+            expect(req.isNew).to.equal(intentRequest.session.new);
+        });
+
+        it('should have a "version" field', function() {
+            expect(req.version).to.equal(intentRequest.version);
+        });
+
+        it('should have a "session" field', function() {
+            expect(req.session).to.deep.equal({});
+        });
+
+        it('should have a "params" field', function() {
+            expect(req.params).to.deep.equal(intentRequest.request.intent.slots);
+        });
+    });
+
+    describe('"say" function', function() {
+
+        it('should chain', function() {
+            expect(req.say("foo")).to.equal(req);
+        });
+
+        it('should accept text', function() {
+            req.say("foo");
+            expect(req.toJSON().response.outputSpeech).to.deep.equal({
+                type: "PlainText",
+                text: "foo"
+            });
+        });
+
+        it('should accept ssml', function() {
+            req.say(<speak />);
+            expect(req.toJSON().response.outputSpeech).to.deep.equal({
+                type: "SSML",
+                text: "<speak></speak>\n"
+            });
+        });
+    });
+
+    describe.skip('"show" function', function() {
+
+        it('should chain', function() {
+            expect(req.show("foo")).to.equal(req);
+        });
+
+        it('should set the title and content of the card', function() {
+
+        });
+    });
+
+    describe('"reprompt" function', function() {
+
+        it('should chain', function() {
+            expect(req.reprompt("foo")).to.equal(req);
+        });
+
+        it('should accept text', function() {
+            req.reprompt("foo");
+            expect(req.toJSON().response.reprompt.outputSpeech).to.deep.equal({
+                type: "PlainText",
+                text: "foo"
+            });
+        });
+
+        it('should accept ssml', function() {
+            req.reprompt(<speak />);
+            expect(req.toJSON().response.reprompt.outputSpeech).to.deep.equal({
+                type: "SSML",
+                text: "<speak></speak>\n"
+            });
+        });
+    });
+
+    describe('"end" function', function() {
+        it('should chain', function() {
+            expect(req.end("foo")).to.equal(req);
+        });
+
+        it('should ', function() {
+            req.end();
+            expect(req.toJSON().response.shouldEndSession).to.equal(true);
+        })
+    });
+
+    describe('"toJSON" function', function() {
+
+        it('should work', function() {
+            req.say("foo").show("foo").reprompt(<speak>foo</speak>).end();
+            expect(req.toJSON()).to.deep.equal(intentResponse);
+        });
+    });
+});
