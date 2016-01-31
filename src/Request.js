4093c28a16fba136cd740147bc3e597cf0d7035e
Nicholas Clawson <nickclaw@gmail.com>
2016-01-30 17:54:09 -0800

    Fix session handling, improve how reponses are created

--- src/Request.js
+++ src/Request.js
@@ -5,62 +5,54 @@ import { renderToString } from 'alexa-ssml';
 export class Request {
 
     constructor(event) {
-        this._event = event;
-
+        this.raw = event;
         this.isNew = get(event, 'session.new', false);
         this.version = get(event, 'version', '1.0');
         this.session = get(event, 'session.session', {});
         this.params = get(event, 'request.intent.slots', {});
 
-        this._speech = null;
-        this._card = null;
-        this._reprompt = null;
-        this._end = false;
+        this._res = {
+            outputSpeech: null,
+            card: null,
+            reprompt: null,
+            shouldEndSession: false
+        };
     }
 
     say(content) {
-        this._speech = content;
+        this._res.outputSpeech = toSpeechResponse(content);
         return this;
     }
 
     show(content) {
-        this._card = content;
+        this._res.card = {
+            type: "Simple",
+            title: "Response",
+            content: content
+        };
         return this;
     }
 
     reprompt(content) {
-        this._reprompt = content;
+        this._res.reprompt = {
+            outputSpeech: toSpeechResponse(content)
+        };
         return this;
     }
 
     end() {
-        this._end = true;
+        this._res.shouldEndSession = true;
         return this;
     }
 
     toJSON() {
-        const data = {
-            version: this.version,
-            sessionAttributes: this.session,
-            response: { shouldEndSession: this._end }
+        const { version, session, _res: response } = this;
+
+        return {
+            version: version,
+            response: response,
+            sessionAttributes: { session }
         };
-
-        if (this._speech)
-            data.response.outputSpeech = toSpeechResponse(this._speech);
-
-        if (this._card)
-            data.response.card = {
-                type: "Simple",
-                title: "Response",
-                content: this._card
-            };
-
-        if (this._reprompt)
-            data.response.reprompt = {
-                outputSpeech: toSpeechResponse(this._reprompt)
-            };
-
-        return data;
     }
 }
 
