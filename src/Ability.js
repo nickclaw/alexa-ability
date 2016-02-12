f5c38848d0d0754e176ecb98677dd7d8f8da5425
Nicholas Clawson <nickclaw@gmail.com>
2016-02-11 20:28:36 -0800

    Use setImmediate instead

--- src/Ability.js
+++ src/Ability.js
@@ -86,8 +86,8 @@ export class Ability {
 
         // build request object and attach listeners
         const req = new Request(event);
-        req.on('finished', process.nextTick.bind(null, () => callback(null, req)));
-        req.on('failed', process.nextTick.bind(null, err => callback(err, req)));
+        req.on('finished', () => setImmediate(callback, null, req));
+        req.on('failed', err => setImmediate(callback, err, req));
 
         // iterate over the stack of middleware and handlers
         // kind of like express does
