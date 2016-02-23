07ffdb9b33ed2beeffe2a6cb59daa6165bc46ae7
Nicholas Clawson <nickclaw@gmail.com>
2016-02-22 21:07:15 -0800

    Add basic resolve tests

--- test/resolve-test.js
+++ test/resolve-test.js
@@ -1,10 +1,40 @@
 import { resolve } from '../src/resolve';
 
 const req = {};
-const val = {};
 const err = new Error();
 
 describe('resolve(fn, ...args, done)', function() {
 
+    it('should handle functions that throw synchronously', function(done) {
+        const handler = sinon.spy((req, next) => { throw err });
+        const callback = (e) => {
+            expect(e).to.equal(err);
+            done();
+        };
 
+        resolve(handler, callback, req);
+        expect(handler).to.be.calledWith(req, callback);
+    });
+
+    it('should handle functions that resolve a value node-style', function(done) {
+        const handler = sinon.spy((req, next) => next(null, null));
+        const callback = (e) => {
+            expect(e).to.equal(null);
+            done();
+        };
+
+        resolve(handler, callback, req);
+        expect(handler).to.be.calledWith(req, callback);
+    });
+
+    it('should handle functions that pass an error node-style', function(done) {
+        const handler = sinon.spy((req, next) => next(err));
+        const callback = (e) => {
+            expect(e).to.equal(err);
+            done();
+        };
+
+        resolve(handler, callback, req);
+        expect(handler).to.be.calledWith(req, callback);
+    });
 });
